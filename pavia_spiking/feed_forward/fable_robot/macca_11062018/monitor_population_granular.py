11f596a72104922eb50d34b7a3a6d875fbaca6af
mc-capolei <marieclaire.capolei@icloud.com>
2018-06-11 13:06:21 +0200

    Delete monitor_population_granular.py

--- pavia_spiking/feed_forward/fable_robot/macca_11062018/monitor_population_granular.py
+++
@@ -1,7 +0,0 @@
-# This specifies that the neurons of the motor population
-# should be monitored. You can see them in the spike train widget
-@nrp.NeuronMonitor(nrp.brain.GRANULAR, nrp.spike_recorder)
-def monitor_population_granular(t):
-    # Uncomment to log into the 'log-console' visible in the simulation
-    # clientLogger.info("Time: ", t)
-    return True
