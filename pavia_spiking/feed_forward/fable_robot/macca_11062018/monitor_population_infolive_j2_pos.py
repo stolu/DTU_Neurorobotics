f083ab1b82aea47c5703b1713a880e7aacaa1726
mc-capolei <marieclaire.capolei@icloud.com>
2018-06-11 13:06:48 +0200

    Delete monitor_population_infolive_j2_pos.py

--- pavia_spiking/feed_forward/fable_robot/macca_11062018/monitor_population_infolive_j2_pos.py
+++
@@ -1,7 +0,0 @@
-# This specifies that the neurons of the motor population
-# should be monitored. You can see them in the spike train widget
-@nrp.NeuronMonitor(nrp.brain.INFOLIVE_j2, nrp.spike_recorder)
-def monitor_population_io2pos(t):
-    # Uncomment to log into the 'log-console' visible in the simulation
-    # clientLogger.info("Time: ", t)
-    return True
