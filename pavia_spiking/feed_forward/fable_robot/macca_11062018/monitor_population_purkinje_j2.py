9497dcf5847a86bca560070fd15be00eb990d080
mc-capolei <marieclaire.capolei@icloud.com>
2018-06-11 13:07:18 +0200

    Delete monitor_population_purkinje_j2.py

--- pavia_spiking/feed_forward/fable_robot/macca_11062018/monitor_population_purkinje_j2.py
+++
@@ -1,7 +0,0 @@
-# This specifies that the neurons of the motor population
-# should be monitored. You can see them in the spike train widget
-@nrp.NeuronMonitor(nrp.brain.PURKINJE_j2, nrp.spike_recorder)
-def monitor_population_purkinje2(t):
-    # Uncomment to log into the 'log-console' visible in the simulation
-    # clientLogger.info("Time: ", t)
-    return True
